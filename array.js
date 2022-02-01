14875112c8260e623896f1f51c42974f698a5240
vivekyadav1998 <yadavivek20@gmail.com>
2022-02-01 22:05:18 +0530

    objects

---
+++ array.js
@@ -0,0 +1,14 @@
+let arr=[1,2,3,4,5,6]
+console.log(arr);
+
+let arr2=[1,'a',"vivek",true];
+console.log(arr2);
+
+arr2[0]=5;
+console.log(arr2);
+
+arr2[6]="new"
+console.log(arr2);
+
+arr2[4]="second";
+console.log(arr2);
